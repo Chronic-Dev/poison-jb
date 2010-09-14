cb8573eef1d4dfb11e79eeef7d2f75982da34365
Joshua Hill <posixninja@storm.(none)>
2010-09-14 19:19:59 -0400

    more work on patching routines done

--- payload/patch.c
+++ payload/patch.c
@@ -13,14 +13,14 @@
 #include "common.h"
 #include "commands.h"
 
-const unsigned char patch_cert_seq[] = "\x1a\x78\xff\x2a\x4f\xf0\xff\x30";
-const unsigned char patch_cert[] = "\x00\x20\x00\x20";
+const unsigned char* patch_cert_seq = "\x4F\xF0\xFF\x30\xDD\xF8\x40\x24";
+const unsigned char* patch_cert = "\x00\x20\x00\x20";
 
 const unsigned char patch_perm_seq[] = "\xf3\xdf\x90\xb5\x07\x4b\x1b\x68";
 const unsigned char patch_perm[] = "\x4f\xf0\xff\x33";
 
-const unsigned char patch_tags_seq[] = "\x11\x9a\xd3\xf1\x18\xbf\x01\x20";
-const unsigned char patch_tags[] = "\x00\x20\x00\x20";
+const unsigned char patch_ecid_seq[] = "\x02\x94\x03\x94\x01\x90\x28\x46";
+const unsigned char patch_ecid[] = "\x00\x20\x00\x20";
 
 const unsigned char patch_bgcolor_seq[] = "\x80\xb5\x00\xaf\x04\x28\x0b\x46";
 const unsigned char patch_bgcolor[] = "\x00\x4b\x18\x47\x00\x00\x00\x41";
@@ -52,20 +52,53 @@ int patch_kernel(void* address, int size) {
 }
 
 int patch_firmware(unsigned char* address, int size) {
-	unsigned char* cert_offset = patch_find(address, size, patch_cert_seq, sizeof(patch_cert_seq));
+	unsigned char* cert_offset = patch_find(address, size, patch_cert_seq, 8);
 	if(cert_offset == NULL) {
 		printf("Unable to find RSA patch offset\n");
 		return -1;
 	}
-	cert_offset += 4;
 	printf("Found RSA patch offset at %p\n", cert_offset);
-	SETREG32(cert_offset, patch_cert);
+	memcpy(cert_offset, patch_cert, 4);
+
+	unsigned char* ecid_offset = patch_find(address, size, patch_ecid_seq, 8);
+	if(ecid_offset == NULL) {
+		printf("Unable to find ECID patch offset\n");
+		return -1;
+	}
+	ecid_offset += 8;
+	printf("Found ECID patch offset at %p\n", ecid_offset);
+	memcpy(ecid_offset, patch_ecid, 4);
+
+	unsigned char* getenv_string = patch_find(address, size, "getenv", 6);
+	if(getenv_string == NULL) {
+		printf("Unable to find getenv string\n");
+		return -1;
+	}
+	printf("Found getenv string at %p\n", getenv_string);
+
+	unsigned char* getenv_xref = patch_find(address, size, (unsigned char*)&getenv_string, 4);
+	if(getenv_string == NULL) {
+		printf("Unable to find getenv xref\n");
+		return -1;
+	}
+	printf("Found getenv xref at %p\n", getenv_xref);
+
 	return 0;
 }
 
-unsigned char* patch_find(unsigned char* start, int length, const unsigned char find[], int size) {
+unsigned char* patch_find(unsigned char* start, int length, unsigned char* find, int size) {
 	int i = 0;
 	for(i = 0; i < length; i++) {
+		if(!memcmp(&start[i], find, size)) {
+			return &start[i];
+		}
+	}
+	return NULL;
+}
+
+unsigned char* patch_rfind(unsigned char* start, int length, unsigned char* find, int size) {
+	int i = 0;
+	for(i = length; i < length; i--) {
 		if(!memcmp(&start[i], find, size)) {
 			return &start[i];
 		}
