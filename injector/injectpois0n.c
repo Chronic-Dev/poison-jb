d8c82dc204f86ba046d616e1496b06d36dd89d20
pod2g <pod2g@pod2g-asus.(none)>
2010-09-16 19:18:30 +0200

    enabled back relocation

--- injector/injectpois0n.c
+++ injector/injectpois0n.c
@@ -208,9 +208,9 @@ int execute_payload() {
 
 	debug("Loading and patching iBoot\n");
 	irecv_send_command(client, "go");
-	irecv_send_command(client, "go image load 0x69626F74 0x42000000");
-	irecv_send_command(client, "go patch 0x42000000 0x38000");
-	irecv_send_command(client, "go jump 0x42000040");
+	irecv_send_command(client, "go image load 0x69626F74 0x41000000");
+	irecv_send_command(client, "go patch 0x41000000 0x38000");
+	irecv_send_command(client, "go jump 0x41000040");
 
 	debug("Reconnecting to device\n");
 	client = irecv_reconnect(client);
@@ -224,9 +224,9 @@ int execute_payload() {
 
 	// it dunt werk!!! =(
 	//irecv_send_command(client, "go");
-	//irecv_send_command(client, "go kernel load 0x43000000");
-	//irecv_send_command(client, "go patch 0x42000000 0x38000");
-	//irecv_send_command(client, "go kernel patch 0x43000000 10186752");
+	//irecv_send_command(client, "go kernel load 0x42000000");
+	//irecv_send_command(client, "go patch 0x41000000 0x38000");
+	//irecv_send_command(client, "go kernel patch 0x42000000 10186752");
 
 	return 0;
 }
