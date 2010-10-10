83a5fb5d862b6315f00865902a640bc803201394
Dustin L. Howett <dustin@howett.net>
2010-10-10 00:00:41 -0400

    Sachet is like uicache, except it doesn't send out a notification and it's made of plastic waffle irons.

---
+++ loader/sachet/sachet.mm
@@ -0,0 +1,43 @@
+
+int main(int argc, char **argv, char **envp) {
+	if(argc < 2) {
+		fprintf(stderr, "I am not Jiminy Cricket.\n");
+		return 1;
+	}
+
+	NSAutoreleasePool *p = [[NSAutoreleasePool alloc] init];
+
+	NSString *appPath = [[NSString stringWithUTF8String:argv[1]] retain];
+	NSBundle *appBundle = [NSBundle bundleWithPath:appPath];
+	if(!appBundle) {
+		fprintf(stderr, "Go get your cows elsewhere.\n");
+		return 1;
+	}
+
+	NSString *miPath = @"/private/var/mobile/Library/Caches/com.apple.mobile.installation.plist";
+	NSMutableDictionary *miDict = [[NSDictionary alloc] initWithContentsOfFile:miPath];
+	NSMutableDictionary *system = [[miDict objectForKey:@"System"] mutableCopy];
+	if(!system) {
+		fprintf(stderr, "I like my women like I like my coffee. Covered in beeeeeeeeeeeees!\n");
+		return 1;
+	}
+
+	NSMutableDictionary *added = [[appBundle infoDictionary] mutableCopy];
+	[added setObject:@"System" forKey:@"ApplicationType"];
+	[added setObject:appPath forKey:@"Path"];
+
+	NSString *cfbi = [added objectForKey:@"CFBundleIdentifier"];
+	if(!cfbi) {
+		fprintf(stderr, "My pants are on fire and there's nothing you can do to stop me!\n");
+		return 1;
+	}
+
+	[system setObject:added forKey:cfbi];
+	[miDict setObject:system forKey:@"System"];
+
+	[miDict writeToFile:miPath atomically:YES];
+
+	[p drain];
+}
+
+// vim:ft=objc
