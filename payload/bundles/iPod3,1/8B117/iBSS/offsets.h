3ffdc3fdf85ee65420db63f4ecaf6904be0c2f41
pod2g <pod2g@pod2g-asus.(none)>
2010-10-11 00:54:54 +0200

    iPod3,1 iBSS bundle

--- payload/bundles/iPod3,1/8B117/iBSS/offsets.h
+++ payload/bundles/iPod3,1/8B117/iBSS/offsets.h
@@ -24,29 +24,29 @@
 #define TARGET_BASEADDR                   (0x84000000)
 
 // Standard offsets
-#define TARGET_FREE                       (0xDEAD+1)
-#define TARGET_MALLOC                     (0xDEAD+1)
-#define TARGET_JUMP_TO                    (0xDEAD+1)
-#define TARGET_PRINTF                     (0xDEAD+1)
-#define TARGET_VPRINTF                    (0xDEAD+1)
+#define TARGET_FREE                       (0xE048+1)
+#define TARGET_MALLOC                     (0xE2CC+1)
+#define TARGET_JUMP_TO                    (0x121AC+1)
+#define TARGET_PRINTF                     (0x17664+1)
+#define TARGET_VPRINTF                    (0x16F34+1)
 
 // Command offsets
-#define TARGET_CMD_LIST_BEGIN             (0xDEAD)
-#define TARGET_CMD_LIST_END               (0xDEAD)
+#define TARGET_CMD_LIST_BEGIN             (0x1E000)
+#define TARGET_CMD_LIST_END               (0x1E02C)
 
 // Task functions
-#define TARGET_TASK_YIELD                 (0xDEAD+1)
-#define TARGET_TASK_RUNNING               (0xDEAD)
-#define TARGET_TASK_LIST                  (0xDEAD)
+#define TARGET_TASK_YIELD                 (0x12AA8+1)
+#define TARGET_TASK_RUNNING               (0x1E984)
+#define TARGET_TASK_LIST                  (0x1EA1C)
 
 // AES offsets
-#define TARGET_AES_CRYPTO_CMD             (0xDEAD+1)
+#define TARGET_AES_CRYPTO_CMD             (0x13CE8+1)
 
 // BDev offsets
-#define TARGET_BDEV_LIST                  (0xDEAD)
+#define TARGET_BDEV_LIST                  (0x21680)
 
 // Image offsets
-#define TARGET_IMAGE_LIST                 (0xDEAD)
+#define TARGET_IMAGE_LIST                 (0x1E954)
 
 // Filesystem offsets
 //#define TARGET_FS_MOUNT                   (0x0)
@@ -54,11 +54,11 @@
 //#define TARGET_FS_LOAD_FILE               (0x0)
 
 // Kernel offsets
-#define TARGET_KERNEL_LOAD                (0xDEAD+1)
-#define TARGET_KERNEL_PHYMEM              (0xDEAD)
-#define TARGET_KERNEL_BOOTARGS            (0xDEAD)
+#define TARGET_KERNEL_LOAD                (0xF5E4+1)
+#define TARGET_KERNEL_PHYMEM              (0x21BC0)
+#define TARGET_KERNEL_BOOTARGS            (0x1BA04)
 
 // NVRAM offsets
-#define TARGET_NVRAM_LIST                 (0xDEAD)
+#define TARGET_NVRAM_LIST                 (0x1EA5C)
 
 #endif // OFFSETS_H
