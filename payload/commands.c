db2a0ba69e303d1f58b669d672ef40434c234399
pod2g <pod2g@pod2g-asus.(none)>
2010-10-11 02:01:08 +0200

    fix hardcoded iboot offset

--- payload/commands.c
+++ payload/commands.c
@@ -255,16 +255,16 @@ int cmd_fsboot(int argc, CmdArg* argv) {
 	}
 
 	// search for jump_to function               80  B5  00  AF  04  46  15  46
-	jump_to = patch_find(0x5ff00000, 0x30000, "\x80\xb5\x00\xaf\x04\x46\x15\x46", 8);
+	jump_to = patch_find(IBOOT_BASEADDR, 0x30000, "\x80\xb5\x00\xaf\x04\x46\x15\x46", 8);
 	//printf("Found jump_to function at %p\n", jump_to);
 
 	memcpy(jump_to, "\x00\x4b\x98\x47", 4);
 	memcpy(jump_to+4, &hooker, 4);
 	//printf("Hooked jump_to function to call 0x%08x\n", hooker);
-	if(strstr((char*) 0x5ff00200, "k66ap")) {
-		fsboot = patch_find(0x5ff00000, 0x30000, "\xf0\xb5\x03\xaf\x81\xb0", 6);
+	if(strstr((char*) (IBOOT_BASEADDR + 0x200), "k66ap")) {
+		fsboot = patch_find(IBOOT_BASEADDR, 0x30000, "\xf0\xb5\x03\xaf\x81\xb0", 6);
 	} else {
-		fsboot = patch_find(0x5ff00000, 0x30000, "\xb0\xb5\x02\xaf\x11\x48", 6);
+		fsboot = patch_find(IBOOT_BASEADDR, 0x30000, "\xb0\xb5\x02\xaf\x11\x48", 6);
 	}
 	//printf("Found fsboot function at %p\n", fsboot);
 
