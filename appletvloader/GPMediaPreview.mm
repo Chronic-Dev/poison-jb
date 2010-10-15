ee5a5bebb63ec7764f92c4cb843acc46d7d79627
lechium <kevin.w.bradley@me.com>
2010-10-14 21:30:53 -0700

    moved hook function to CPlusFunctions and added BRLocalizedString to Prefix

--- appletvloader/GPMediaPreview.mm
+++ appletvloader/GPMediaPreview.mm
@@ -2,14 +2,7 @@
 
 #import "GPMediaPreview.h"
 #import "GPMedia.h"
-#import <objc/runtime.h>
-
-template <typename Type_>
-static inline Type_ &MSHookIvar(id self, const char *name) {
-    Ivar ivar(class_getInstanceVariable(object_getClass(self), name));
-    void *pointer(ivar == NULL ? NULL : reinterpret_cast<char *>(self) + ivar_getOffset(ivar));
-    return *reinterpret_cast<Type_ *>(pointer);
-}
+#import "CPlusFunctions.mm"
 
 @interface BRCoverArtImageLayer (compat)
 -(id)texture;
@@ -28,10 +21,6 @@ static inline Type_ &MSHookIvar(id self, const char *name) {
 @implementation BRMetadataPreviewControl (protectedAccess)
 - (BRMetadataControl *)gimmieMetadataLayer
 {
-	//Class myClass = [self class];
-	//Ivar ret = class_getInstanceVariable(myClass,"_metadataLayer");
-	
-	//return *(BRMetadataControl * *)(((char *)self)+ret->ivar_offset);
 	return MSHookIvar<BRMetadataControl *>(self, "_metadataLayer");
 }
 @end
