33304150b07a573e3b8f9f3318a9dbd3e5ce4e94
Nicolas Haunold <westbaer@gmail.com>
2010-10-17 23:40:57 +0200

    setuid(0)

--- appletvloader/wuntar/main.mm
+++ appletvloader/wuntar/main.mm
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "tar.h"
 
 int main(int argc, char *argv[]) {
+    setuid(0);
+
     if(argc < 3) {
 	printf("%s <archive> <path>\n", argv[0]);
 	printf("\nCopyright 2010 Nicolas Haunold.\n'tar.h' by Elliot Kroo.\n");
